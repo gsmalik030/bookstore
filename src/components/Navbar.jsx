2b55b5cef25e567f6d1a5e4db4a5acf180e735a9
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-03 06:50:52 +0200

    Changed links style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import accIcon from '../assets/images/profile.png';
-import "./styles/navBar.css"
+import './styles/navBar.css';
 const Navbar = () => (
   <nav className='navBar'>
-    <div className="navContainer">
+    <div className='navContainer'>
       <p className='logo'>Bookstore CMS</p>
       <ul className='navLinks'>
-        <li><Link to="/">Books</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
+        <li>
+          <Link className='navLink' to='/'>
+            Books
+          </Link>
+        </li>
+        <li>
+          <Link className='navLink oppacity' to='/categories'>
+            Categories
+          </Link>
+        </li>
       </ul>
     </div>
-    <img src={accIcon} alt="profile" />
+    <img src={accIcon} alt='profile' />
   </nav>
 );
 
