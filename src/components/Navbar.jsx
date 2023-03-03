97f8b5d77ae67e87f6016d194e34d14658e75fd4
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-03 06:02:09 +0200

    Changed styles as per request

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {MdAccountCircle} from 'react-icons/md';
+import accIcon from '../assets/images/profile.png';
 import "./styles/navBar.css"
 const Navbar = () => (
   <nav className='navBar'>
     <div className="navContainer">
       <p className='logo'>Bookstore CMS</p>
       <ul className='navLinks'>
-        <li className='navlink'><Link to="/">Books</Link></li>
+        <li><Link to="/">Books</Link></li>
         <li><Link to="/categories">Categories</Link></li>
       </ul>
     </div>
-    <div className="ovel">
-    <MdAccountCircle className='accIcon' />
-    </div>
+    <img src={accIcon} alt="profile" />
   </nav>
 );
 
